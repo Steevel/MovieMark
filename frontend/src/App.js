a2589de64a2f21aebe200f4ded58ec01ab52c669
Steevel <57296025+Steevel@users.noreply.github.com>
2023-03-05 16:07:24 +0530

    Configured eslint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,9 @@
-import "./App.css";
+import './App.css';
 
 function App() {
-	return (
-		<>
-			<h1>App Component</h1>
-		</>
-	);
+  return (
+    <h1>App Component</h1>
+  );
 }
 
 export default App;
